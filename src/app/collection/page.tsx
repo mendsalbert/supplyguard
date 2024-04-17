f3f1448b5a603ea1005bea88c198dc3442ba3811
Albert Mends <mendsalbert@gmail.com>
2024-04-17 09:30:16 +0000

    update page.tsx

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -42,15 +42,6 @@ const PageCollection = ({}) => {
             </div>
           </main>
         </div>
-
-        {/* === SECTION 5 === */}
-        <hr className="border-slate-200 dark:border-slate-700" />
-
-        <SectionSliderCollections />
-        <hr className="border-slate-200 dark:border-slate-700" />
-
-        {/* SUBCRIBES */}
-        <SectionPromo1 />
       </div>
     </div>
   );

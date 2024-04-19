b229061c8b6a6e503d35fcd6f3d81d045d04ffc8
Albert Mends <mendsalbert@gmail.com>
2024-04-19 08:19:14 +0000

    update page.tsx

--- src/app/(accounts)/admin-account-dashboard/page.tsx
+++ src/app/(accounts)/admin-account-dashboard/page.tsx
@@ -1,94 +1,64 @@
-import Prices from "@/components/Prices";
+import ProductCard from "@/components/ProductCard";
 import { PRODUCTS } from "@/data/data";
 import ButtonSecondary from "@/shared/Button/ButtonSecondary";
-import Image from "next/image";
+import {
+  CheckCircleIcon,
+  CurrencyDollarIcon,
+  RectangleGroupIcon,
+  UsersIcon,
+} from "@heroicons/react/24/outline";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 
-const AccountOrder = () => {
-  const renderProductItem = (product: any, index: number) => {
-    const { image, name } = product;
+const AccountSavelists = () => {
+  const renderSection1 = () => {
     return (
-      <div key={index} className="flex py-4 sm:py-7 last:pb-0 first:pt-0">
-        <div className="relative h-24 w-24 flex-shrink-0 overflow-hidden rounded-xl bg-slate-100">
-          <Image
-            fill
-            sizes="100px"
-            src={image}
-            alt={name}
-            className="h-full w-full object-cover object-center"
-          />
+      <div className="space-y-10 sm:space-y-12">
+        <div className="grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 ">
+          <div className="py-8 px-4 ring-1 ring-gray-300 bg-white rounded-lg shadow-sm ">
+            <div className="text-base text-gray-400 ">Net Revenue</div>
+            <div className="flex items-center justify-between pt-1">
+              <div className="text-2xl font-bold text-gray-900 ">$7520.50</div>
+              <CurrencyDollarIcon className="w-9 h-9 text-gray-700" />
+            </div>
+          </div>
+          <div className="py-8 px-4 ring-1 ring-gray-300 bg-white rounded-lg shadow-sm ">
+            <div className="text-base text-gray-400 ">Customers</div>
+            <div className="flex items-center justify-between pt-1">
+              <div className="text-2xl font-bold text-gray-900 ">1375</div>
+              <UsersIcon className="w-9 h-9 text-gray-700" />
+            </div>
+          </div>
+
+          <div className="py-8 px-4 ring-1 ring-gray-300 bg-white rounded-lg shadow-sm ">
+            <div className="text-base text-gray-400 ">Total Products</div>
+            <div className="flex items-center justify-between pt-1">
+              <div className="text-2xl font-bold text-gray-900 ">14,754</div>
+              <RectangleGroupIcon className="w-9 h-9 text-gray-700" />
+            </div>
+          </div>
+          <div className="py-8 px-4 ring-1 ring-gray-300 bg-white rounded-lg shadow-sm ">
+            <div className="text-base text-gray-400 ">Successful Orders</div>
+            <div className="flex items-center justify-between pt-1">
+              <div className="text-2xl font-bold text-gray-900 ">14,754</div>
+              <CheckCircleIcon className="w-9 h-9 text-gray-700" />
+            </div>
+          </div>
+          <div className="py-8 px-4 ring-1 ring-gray-300 bg-white rounded-lg shadow-sm ">
+            <div className="text-base text-gray-400 ">Unsuccessful Orders</div>
+            <div className="flex items-center justify-between pt-1">
+              <div className="text-2xl font-bold text-gray-900 ">14,754</div>
+              <XCircleIcon className="w-9 h-9 text-gray-700" />
+            </div>
+          </div>
         </div>
-
-        <div className="ml-4 flex flex-1 flex-col">
-          <div>
-            <div className="flex justify-between ">
-              <div>
-                <h3 className="text-base font-medium line-clamp-1">{name}</h3>
-                <p className="mt-1 text-sm text-slate-500 dark:text-slate-400">
-                  <span>{"Natural"}</span>
-                  <span className="mx-2 border-l border-slate-200 dark:border-slate-700 h-4"></span>
-                  <span>{"XL"}</span>
-                </p>
-              </div>
-              <Prices className="mt-0.5 ml-2" />
-            </div>
-          </div>
-          <div className="flex flex-1 items-end justify-between text-sm">
-            <p className="text-gray-500 dark:text-slate-400 flex items-center">
-              <span className="hidden sm:inline-block">Qty</span>
-              <span className="inline-block sm:hidden">x</span>
-              <span className="ml-2">1</span>
-            </p>
-
-            <div className="flex">
-              <button
-                type="button"
-                className="font-medium text-indigo-600 dark:text-primary-500 "
-              >
-                Leave review
-              </button>
-            </div>
-          </div>
+        <div className="flex !mt-20 justify-center items-center">
+          {/* <ButtonSecondary loading>Show me more</ButtonSecondary> */}
         </div>
       </div>
     );
   };
 
-  const renderOrder = () => {
-    return (
-      <div className="border border-slate-200 dark:border-slate-700 rounded-lg overflow-hidden z-0">
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center p-4 sm:p-8 bg-slate-50 dark:bg-slate-500/5">
-          <div>
-            <p className="text-lg font-semibold">#WU3746HGG12</p>
-            <p className="text-slate-500 dark:text-slate-400 text-sm mt-1.5 sm:mt-2">
-              <span>Aug 8, 2023</span>
-              <span className="mx-2">·</span>
-              <span className="text-primary-500">Delivered</span>
-            </p>
-          </div>
-          <div className="mt-3 sm:mt-0">
-            <ButtonSecondary
-              sizeClass="py-2.5 px-4 sm:px-6"
-              fontSize="text-sm font-medium"
-            >
-              View Order
-            </ButtonSecondary>
-          </div>
-        </div>
-        <div className="border-t border-slate-200 dark:border-slate-700 p-2 sm:p-8 divide-y divide-y-slate-200 dark:divide-slate-700">
-          {[PRODUCTS[0], PRODUCTS[1], PRODUCTS[2]].map(renderProductItem)}
-        </div>
-      </div>
-    );
-  };
-
-  return (
-    <div className="space-y-10 sm:space-y-12">
-      {/* HEADING */}
-      <h2 className="text-2xl sm:text-3xl font-semibold">Order History</h2>
-      {renderOrder()}
-      {renderOrder()}
-    </div>
-  );
+  return renderSection1();
 };
 
-export default AccountOrder;
+export default AccountSavelists;

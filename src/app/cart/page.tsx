8fc8a7f19b314f3432392aa4e769e3dc3fb52285
Albert Mends <mendsalbert@gmail.com>
2024-04-26 19:49:48 +0000

    update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { FC, useState, useEffect, useRef } from "react";
 import { NoSymbolIcon, CheckIcon } from "@heroicons/react/24/outline";
 import NcInputNumber from "@/components/NcInputNumber";
 import Prices from "@/components/Prices";
@@ -5,8 +7,29 @@ import { Product, PRODUCTS } from "@/data/data";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
 import Image from "next/image";
 import Link from "next/link";
-
+import imageUrlBuilder from "@sanity/image-url";
+import { client } from "@/api/client";
+import { useAppDispatch, useAppSelector } from "@/app/store";
+import { selectCurrentCart } from "@/features/user/userSlice";
 const CartPage = () => {
+  const dispatch = useAppDispatch();
+  const reduxCart = useAppSelector(selectCurrentCart);
+
+  const builder = imageUrlBuilder(client);
+
+  function urlFor(source: any) {
+    return builder.image(source);
+  }
+  const [quantityValues, setQuantityValues] = useState<{
+    [key: string]: number;
+  }>({});
+
+  const handleChangeQuantity = (itemId: string, value: number) => {
+    setQuantityValues((prevValues) => ({
+      ...prevValues,
+      [itemId]: value,
+    }));
+  };
   const renderStatusSoldout = () => {
     return (
       <div className="rounded-full flex items-center justify-center px-2.5 py-1.5 text-xs text-slate-700 dark:text-slate-300 border border-slate-200 dark:border-slate-700">
@@ -25,9 +48,79 @@ const CartPage = () => {
     );
   };
 
-  const renderProduct = (item: Product, index: number) => {
-    const { image, price, name } = item;
+  const renderSubtotal = () => {
+    const subtotal = reduxCart
+      ?.filter((item: any) => item !== null) // Filter out items with null fields
+      .reduce(
+        (acc: number, item: any) =>
+          acc + (Number(item?.price) || 0) * (quantityValues[item._id] || 0),
+        0
+      );
 
+    return (
+      <div className="flex justify-between pb-4">
+        <span>Subtotal</span>
+        <span className="font-semibold text-slate-900 dark:text-slate-200">
+          ${subtotal.toFixed(2)}
+        </span>
+      </div>
+    );
+  };
+
+  const renderShippingEstimate = () => {
+    // You can calculate shipping estimate here based on your business logic
+    const shippingEstimate = 5.0;
+
+    return (
+      <div className="flex justify-between py-4">
+        <span>Shipping estimate</span>
+        <span className="font-semibold text-slate-900 dark:text-slate-200">
+          ${shippingEstimate}
+        </span>
+      </div>
+    );
+  };
+
+  const renderTaxEstimate = () => {
+    // You can calculate tax estimate here based on your business logic
+    const taxEstimate = 24.9;
+
+    return (
+      <div className="flex justify-between py-4">
+        <span>Tax estimate</span>
+        <span className="font-semibold text-slate-900 dark:text-slate-200">
+          ${taxEstimate}
+        </span>
+      </div>
+    );
+  };
+
+  const renderOrderTotal = () => {
+    const subtotal = reduxCart
+      ?.filter((item: any) => item !== null) // Filter out items with null fields
+      .reduce(
+        (acc: number, item: any) =>
+          acc + (Number(item?.price) || 0) * (quantityValues[item._id] || 0),
+        0
+      );
+
+    // You can calculate order total here based on subtotal, shipping estimate, tax estimate, etc.
+    const shippingEstimate = 5.0;
+    const taxEstimate = 24.9;
+    const orderTotal = subtotal + shippingEstimate + taxEstimate;
+
+    return (
+      <div className="flex justify-between font-semibold text-slate-900 dark:text-slate-200 text-base pt-4">
+        <span>Order total</span>
+        <span>${orderTotal.toFixed(2)}</span>
+      </div>
+    );
+  };
+
+  const renderProduct = (item: any, index: number) => {
+    const { name, price, supplier, description, status, image, _id } = item;
+    const quantity = quantityValues[_id] || 1;
+    const totalPrice = price * quantity;
     return (
       <div
         key={index}
@@ -36,12 +129,12 @@ const CartPage = () => {
         <div className="relative h-36 w-24 sm:w-32 flex-shrink-0 overflow-hidden rounded-xl bg-slate-100">
           <Image
             fill
-            src={image}
-            alt={name}
+            src={(image?.asset && urlFor(image.asset).url()) || ""}
+            alt={name || ""}
             sizes="300px"
             className="h-full w-full object-contain object-center"
           />
-          <Link href="/product-detail" className="absolute inset-0"></Link>
+          <Link href="/product-detail/4" className="absolute inset-0"></Link>
         </div>
 
         <div className="ml-3 sm:ml-6 flex flex-1 flex-col">
@@ -49,93 +142,17 @@ const CartPage = () => {
             <div className="flex justify-between ">
               <div className="flex-[1.5] ">
                 <h3 className="text-base font-semibold">
-                  <Link href="/product-detail">{name}</Link>
+                  <Link href="/product-detail/3">{name}</Link>
                 </h3>
                 <div className="mt-1.5 sm:mt-2.5 flex text-sm text-slate-600 dark:text-slate-300">
                   <div className="flex items-center space-x-1.5">
-                    <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
-                      <path
-                        d="M7.01 18.0001L3 13.9901C1.66 12.6501 1.66 11.32 3 9.98004L9.68 3.30005L17.03 10.6501C17.4 11.0201 17.4 11.6201 17.03 11.9901L11.01 18.0101C9.69 19.3301 8.35 19.3301 7.01 18.0001Z"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M8.35 1.94995L9.69 3.28992"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M2.07 11.92L17.19 11.26"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M3 22H16"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M18.85 15C18.85 15 17 17.01 17 18.24C17 19.26 17.83 20.09 18.85 20.09C19.87 20.09 20.7 19.26 20.7 18.24C20.7 17.01 18.85 15 18.85 15Z"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-
-                    <span>{`Black`}</span>
+                    <span>{supplier?.supplierName}</span>
                   </div>
                   <span className="mx-4 border-l border-slate-200 dark:border-slate-700 "></span>
-                  <div className="flex items-center space-x-1.5">
-                    <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
-                      <path
-                        d="M21 9V3H15"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M3 15V21H9"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M21 3L13.5 10.5"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M10.5 13.5L3 21"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-
-                    <span>{`2XL`}</span>
-                  </div>
                 </div>
 
                 <div className="mt-3 flex justify-between w-full sm:hidden relative">
-                  <select
+                  {/* <select
                     name="qty"
                     id="qty"
                     className="form-select text-sm rounded-md py-1 border-slate-200 dark:border-slate-700 relative z-10 dark:bg-slate-800 "
@@ -147,29 +164,30 @@ const CartPage = () => {
                     <option value="5">5</option>
                     <option value="6">6</option>
                     <option value="7">7</option>
-                  </select>
+                  </select> */}
                   <Prices
                     contentClass="py-1 px-2 md:py-1.5 md:px-2.5 text-sm font-medium h-full"
-                    price={price}
+                    price={Number(price * quantity)}
                   />
                 </div>
               </div>
 
               <div className="hidden sm:block text-center relative">
-                <NcInputNumber className="relative z-10" />
+                <NcInputNumber
+                  className="relative z-10"
+                  defaultValue={quantity}
+                  onChange={(value) => handleChangeQuantity(_id, value)}
+                />
               </div>
 
               <div className="hidden flex-1 sm:flex justify-end">
-                <Prices price={price} className="mt-0.5" />
+                <Prices price={totalPrice} className="mt-0.5" />
               </div>
             </div>
           </div>
 
           <div className="flex mt-auto pt-4 items-end justify-between text-sm">
-            {Math.random() > 0.6
-              ? renderStatusSoldout()
-              : renderStatusInstock()}
-
+            {renderStatusInstock()}
             <a
               href="##"
               className="relative z-10 flex items-center mt-3 font-medium text-primary-6000 hover:text-primary-500 text-sm "
@@ -206,20 +224,16 @@ const CartPage = () => {
 
         <div className="flex flex-col lg:flex-row">
           <div className="w-full lg:w-[60%] xl:w-[55%] divide-y divide-slate-200 dark:divide-slate-700 ">
-            {[
-              PRODUCTS[0],
-              PRODUCTS[1],
-              PRODUCTS[2],
-              PRODUCTS[3],
-              PRODUCTS[4],
-            ].map(renderProduct)}
+            {reduxCart
+              ?.filter((item: any) => item !== null) // Filter out items with null fields
+              .map((item: any, index: any) => renderProduct(item, index))}
           </div>
           <div className="border-t lg:border-t-0 lg:border-l border-slate-200 dark:border-slate-700 my-10 lg:my-0 lg:mx-10 xl:mx-16 2xl:mx-20 flex-shrink-0"></div>
           <div className="flex-1">
             <div className="sticky top-28">
               <h3 className="text-lg font-semibold ">Order Summary</h3>
               <div className="mt-7 text-sm text-slate-500 dark:text-slate-400 divide-y divide-slate-200/70 dark:divide-slate-700/80">
-                <div className="flex justify-between pb-4">
+                {/* <div className="flex justify-between pb-4">
                   <span>Subtotal</span>
                   <span className="font-semibold text-slate-900 dark:text-slate-200">
                     $249.00
@@ -240,7 +254,11 @@ const CartPage = () => {
                 <div className="flex justify-between font-semibold text-slate-900 dark:text-slate-200 text-base pt-4">
                   <span>Order total</span>
                   <span>$276.00</span>
-                </div>
+                </div> */}
+                {renderSubtotal()}
+                {renderShippingEstimate()}
+                {renderTaxEstimate()}
+                {renderOrderTotal()}
               </div>
               <ButtonPrimary href="/checkout" className="mt-8 w-full">
                 Checkout

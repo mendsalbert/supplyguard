c6fa812b44a83620ac71cc40f298e5ef7aff5440
Albert Mends <mendsalbert@gmail.com>
2024-05-02 07:27:08 +0000

    delete 3 files and update 1 file

--- navigation.js
+++
@@ -1,7 +0,0 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
-
-export const locales = ["en", "de", "es", "ja"];
-export const localePrefix = "as-needed";
-
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation({ locales, localePrefix });
